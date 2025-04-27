d2db947a8b1fdfc72e3d62c25376131c61ff00fe
Pavel <voroshilow_pavel@mail.ru>
2025-04-27 16:44:43 +0300

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import formater from './formats/Formater.js';
 export default (file1, file2, format = 'stylish') => {
   const data1 = parse(file1);
   const data2 = parse(file2);
-
   const result = gentree(data1, data2);
   const formated = formater(result, format);
   return formated;
