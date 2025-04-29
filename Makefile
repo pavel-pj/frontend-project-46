11e8910ddaeadc6c15c6924add1d1c50992afd9c
Pavel <voroshilow_pavel@mail.ru>
2025-04-29 19:04:30 +0300

    fix1

--- Makefile
+++ Makefile
@@ -6,6 +6,8 @@ test-coverage:
 	NODE_OPTIONS=--experimental-vm-modules npm run coverage
 diff:
 	gendiff __fixtures__/file01.json __fixtures__/file02.json
+diffy:
+	gendiff __fixtures__/file01.yml __fixtures__/file02.yml
 diff2:
 	gendiff __fixtures__/file01.json __fixtures__/file02.json --format plain
 diff3:
--- src/genTree.js
+++ src/genTree.js
@@ -1,56 +1,49 @@
 import _ from 'lodash';
 
-const gentree = (object1, object2) => {
-  const iter = (data1, data2) => {
-    const keys1 = Object.keys(data1);
-    const keys2 = Object.keys(data2);
-    const keys = _.union(keys1, keys2);
-    keys.sort();
+export default (object1, object2) => {
+  const iter = (obj1, obj2) => {
+    const keys = [...new Set(Object.keys({ ...obj1, ...obj2 }))].toSorted();
 
-    const result = keys.map((key) => {
-      if (_.isPlainObject(data1?.[key]) && _.isPlainObject(data2?.[key])) {
+    return keys.map((key) => {
+      if (_.isPlainObject(obj1?.[key]) && _.isPlainObject(obj2?.[key])) {
         return {
           type: 'nested',
           key,
-          value: iter(data1[key], data2[key]),
+          value: iter(obj1[key], obj2[key]),
         };
       }
 
-      if (!Object.hasOwn(data2, key)) {
-        return {
-          type: 'deleted',
-          key,
-          value: data1[key],
-        };
-      }
-
-      if (!Object.hasOwn(data1, key)) {
+      if (!Object.hasOwn(obj1, key)) {
         return {
           type: 'added',
           key,
-          value: data2[key],
+          value: obj2?.[key],
         };
       }
 
-      if (data1[key] !== data2[key]) {
+      if (!Object.hasOwn(obj2, key)) {
+        return {
+          type: 'removed',
+          key,
+          value: obj1?.[key],
+        };
+      }
+
+      if (!_.isEqual(obj1?.[key], obj2?.[key])) {
         return {
           type: 'updated',
           key,
-          value: [data1?.[key], data2?.[key]],
+          value: [obj1?.[key], obj2?.[key]],
         };
       }
 
       return {
-        type: 'matched',
+        type: 'unchanged',
         key,
-        value: data1[key],
+        value: obj1?.[key],
       };
     });
-    return result;
   };
 
-  const diffResult = iter(object1, object2);
-  return diffResult;
+  return iter(object1, object2);
 };
-
-export default gentree;
